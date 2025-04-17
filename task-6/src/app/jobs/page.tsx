4303938843c4f30ac14a29ae832033213d135a1f
Fikiremariyam <dagi123baba@gmail.com>
2025-04-17 23:00:43 +0300

    up to the job lists

--- task-6/src/app/jobs/page.tsx
+++ task-6/src/app/jobs/page.tsx
@@ -21,9 +21,24 @@ const jOBS = async () => {
     
     <div className="h-screen flex items-center justify-center ">
 
-      <div className="bg-white/5 backdrop-blur-md shadow text-black p-4 w-1/2 h-1/2 overflow-auto overflow-hidden">
-        <h1 className="text-10 font-bold mb-6 text-gray-700 drop-shadow-lg">Job Listings</h1>
-        <div className="  flex flex-col gap-6 max-h-full overflow-auto scroll-hide">
+      <div className=" bg-white backdrop-blur-md shadow shadow-2xl text-black p-4 w-[50%]  h-1/2 overflow-auto overflow-hidden">
+       <div className="flex">
+            <div className="flex flex-col ml-4">
+              <h1 className="font-poppins font-black text-[45px] text-[rgba(37,50,75,1)]" > opportunites </h1>
+              <h2 className="mb-5"> showing {jobs.length} results </h2>
+            </div>
+            <div className="mr-15 ml-auto flex items-center gap-2">
+              <span className="text-gray-500">Sort by:</span>
+              <select className="  px-2 py-1">
+              <option value="relevance">Relevance</option>
+              <option value="date">Date</option>
+              <option value="salary">Salary</option>
+              <option value="location">Location</option>
+              <option value="company">Company</option>
+              </select>
+            </div>
+        </div>
+        <div className="   flex flex-col gap-6 max-h-full overflow-auto scroll-hide">
           {jobs.map((job: any, i: number) => (
             <JobCard key={i} job={job} />
           ))}
--- task-6/src/components/jobcard/page.tsx
+++ task-6/src/components/jobcard/page.tsx
@@ -2,45 +2,72 @@ import React from 'react'
 
 
 type Job = {
-    title: string;
-    company: string;
+  title: string;
+  description: string;
+  responsibilities: string[];
+  ideal_candidate: {
+    age: string;
+    gender: string;
+    traits: string[];
+  };
+  when_where: string;
+  about: {
+    posted_on: string;
+    deadline: string;
     location: string;
-    description: string;
-    deadline:string;
+    start_date: string;
+    end_date: string;
+    categories: string[];
+    required_skills: string[];
+  };
+  company: string;
+  image: string;  
   };
    
 const JobCard = ( { job} :{ job:Job } ) => {
   return (
     <div>
      
-      <div className="bg-white shadow h-auto p-6 rounded-3xl border border-gray-200 hover:shadow-md transition  mt-0 gap-2 px-8 py-10 flex flex-col items-start">
-        
+      <div className=" bg-white shadow h-auto p-6 rounded-4xl border border-gray-200 hover:shadow-md transition mt-0 gap-2 px-8 py-10 flex flex-col items-center justify-center mx-auto">
         <div className="flex">
           <div className="w-15 h-15 -mt-3 rounded-full bg-gray-300 flex items-center justify-center">
             <span className="text-gray-700 font-bold text-sm">A</span>
           </div>
 
-          <div className="ml-5 -mt-3 space-y-2">
+          <div className="w-[80%] ml-5 -mt-3 space-y-2">
             <h2 className="text-xl font-semibold text-gray-800">{job.title}</h2>
-            <p className="text-sm text-gray-500 mb-1">{job.company} • <span className="mx-1"></span>{job.location}</p>
-          </div>
-        </div>
-
-        <div className="mt-5  ">
+            <p className="text-sm text-gray-500 mb-1">{job.company} • <span className="mx-1"></span>{job.when_where}</p>
+            <div className="mt-5  ">
           {job.description}
 
         </div>
         
         <div className="mt-5 flex items-center  gap-3 ">
+          <div className=' rounded-2xl border-2 border-green-600 text-green-600 w-20 h-8 w-auto px-4 py-1'>
+            {job.about.location}
+          </div>
+          <h1 className='text-4xl text-gray-200 shadow'> |</h1>
 
-          <button className=' rounded-2xl border-2 border-green-600 text-green-600 w-20 h-8'>
-            IT
-          </button>
-          <button className=' rounded-2xl border-2 border-yellow-400 text-yellow-300 min-w-20 h-8 px-4'>
-            Education
-          </button>
+          {job.about.categories && job.about.categories.map((category, index) => (
+            <button 
+              key={index} 
+              className={`rounded-2xl border-2 px-4 py-1 w-auto ${
+              index % 2 === 0 
+                ? 'border-yellow-300 text-yellow-300' 
+                : 'border-green-600 text-green-600'
+              }`}
+            >
+              {category}
+            </button>
+          ))}
+          
+          
 
         </div>
+          </div>
+        </div>
+
+       
 
 
         
